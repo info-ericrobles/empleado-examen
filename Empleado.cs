287ba1984241c8c75fcf85875967661968daf9e3
info-ericrobles <78887644+info-ericrobles@users.noreply.github.com>
2021-03-23 10:39:17 +0800

    Add files via upload

---
+++ Empleado.cs
@@ -0,0 +1,64 @@
+using System;
+
+class Empleado
+{
+        private string[] empleados;
+        private int[,] sueldos;
+        private int[] sueldostot;
+
+        public void Cargar() 
+        {
+            empleados=new String[5];
+            sueldos=new int[5,1];
+            for(int f = 0; f < empleados.Length; f++)
+            {
+                Console.Write("Ingrese el nombre del empleado:");
+                empleados[f]=Console.ReadLine();
+                for(int c = 0; c < sueldos.GetLength(1); c++) 
+                {
+                    Console.Write("Ingrese sueldo:");
+                    string linea;
+                    linea = Console.ReadLine();
+                    sueldos[f,c]=int.Parse(linea);
+                }
+            }
+        }
+
+        public void CalcularSumaSueldos()
+        {
+            sueldostot = new int[5];
+            for (int f = 0; f < sueldos.GetLength(0); f++)
+            {
+                int suma = 0;
+                for (int c = 0; c < sueldos.GetLength(1); c++)
+                {
+                    suma = suma + sueldos[f,c];
+                }
+                sueldostot[f] = suma;
+            }
+        }
+
+        public void ImprimirTotalPagado() 
+        {
+    	    Console.WriteLine("Total de sueldos pagados por empleado.");
+            for(int f = 0; f < sueldostot.Length; f++) 
+            {
+                Console.WriteLine(empleados[f]+" - "+sueldostot[f]);
+            }
+        }
+
+        public void EmpleadoMayorSueldo() 
+        {
+            int may=sueldostot[0];
+            string nom=empleados[0];
+            for(int f = 0; f < sueldostot.Length; f++) 
+            {
+                if (sueldostot[f] > may) 
+                {
+                    may=sueldostot[f];
+                    nom=empleados[f];
+                }
+            }
+            Console.WriteLine("El empleado con mayor sueldo es "+ nom + " que tiene un sueldo de "+may);
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Matriz9
+{
+    class Matriz9
+    {
+        static void Main(string[] args)
+        {
+            Empleado empleado = new Empleado();
+            empleado.Cargar();
+            empleado.CalcularSumaSueldos();
+            empleado.ImprimirTotalPagado();
+            empleado.EmpleadoMayorSueldo();
+            Console.ReadKey();
+        }
+    }
+}
